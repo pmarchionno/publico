8ee542a25351e121242903e0650e3c882233b94e
Pablo Damian Marchionno <pmarchionno@gmail.com>
2026-03-17 15:38:27 -0300

    Update __manifest__.py

--- l10n_ar_payment_receipt_fix/__manifest__.py
+++ l10n_ar_payment_receipt_fix/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Fix Recibo de Pago - Retenciones Argentina',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Accounting/Localizations',
     'summary': 'Corrige el importe de transferencia en recibos con retenciones',
     'description': '''
